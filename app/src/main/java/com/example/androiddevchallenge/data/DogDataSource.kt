9a915e08033e579f129eda009c7ec258d878e299
Adrián Lázaro <adri92css@gmail.com>
2021-03-01 19:53:24 +0100

    Added Dog entity and data source

---
+++ app/src/main/java/com/example/androiddevchallenge/data/DogDataSource.kt
@@ -0,0 +1,56 @@
+package com.example.androiddevchallenge.data
+
+import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.entities.Dog
+
+class DogDataSource {
+
+    fun getDogs(): List<Dog> {
+        return localDogs()
+    }
+
+    private fun localDogs(): List<Dog> {
+        return listOf(
+            Dog(
+                0,
+                "Puppi",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+                1,
+                "Black",
+                R.drawable.sample_3,
+            ),
+            Dog(
+                0,
+                "Doggy",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+                4,
+                "White",
+                R.drawable.sample_4,
+            ),
+            Dog(
+                0,
+                "Terminator",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+                2,
+                "Brown",
+                R.drawable.sample_5,
+            ),
+            Dog(
+                0,
+                "Biute",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+                5,
+                "Brown",
+                R.drawable.sample_1,
+            ),
+            Dog(
+                0,
+                "Androidy",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+                3,
+                "White",
+                R.drawable.sample_2,
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/example/androiddevchallenge/entities/Dog.kt
@@ -0,0 +1,10 @@
+package com.example.androiddevchallenge.entities
+
+data class Dog(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val age: Int,
+    val color: String,
+    val image: Int
+)
